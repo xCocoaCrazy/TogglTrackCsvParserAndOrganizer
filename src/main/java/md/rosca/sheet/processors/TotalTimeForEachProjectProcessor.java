87fe7262792c680b77cbb94357b14ae4c21db5ed
xCocoaCrazy <xcocoacrazy13@gmail.com>
2024-08-11 08:46:19 +0300

    feat(GenerateChart): Add chart generation for all sheets

--- src/main/java/md/rosca/sheet/processors/TotalTimeForEachProjectProcessor.java
+++ src/main/java/md/rosca/sheet/processors/TotalTimeForEachProjectProcessor.java
@@ -1,49 +1,45 @@
 package md.rosca.sheet.processors;
 
 import md.rosca.TimeEntry;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.util.List;
 import java.util.Map;
 
-import static md.rosca.utils.ExcelUtil.autoSizeColumns;
+import static md.rosca.utils.ChartUtil.createPieChart;
+import static md.rosca.utils.ExcelUtil.*;
 
 public class TotalTimeForEachProjectProcessor {
-    public static void populateTotalTimeForEachProjectTracked(Map<String, List<TimeEntry>> dataByProject, Workbook workbook) {
+    public static void populateTotalTimeForEachProjectTracked(Map<String, List<TimeEntry>> dataByProject, XSSFWorkbook workbook) {
         for (String project : dataByProject.keySet()) {
-            Sheet sheet = getSheet(workbook, project);
+            XSSFSheet sheet = getSheet(workbook, project);
             List<TimeEntry> filteredListOfProject = dataByProject
                     .get(project)
                     .stream()
                     .filter(entry -> project.equals(entry.getProject()))
                     .toList();
 
-            int rowNum = 0;
-            createHeaderRows(sheet.createRow(rowNum++));
+            int numberOfRows = 0;
+            int numberOfColumns = createHeaderRows(sheet.createRow(numberOfRows++), "Project", "Description", "Duration");
             for (TimeEntry entry : filteredListOfProject) {
-                Row row = sheet.createRow(rowNum++);
+                XSSFRow row = sheet.createRow(numberOfRows++);
                 row.createCell(0).setCellValue(entry.getProject());
                 row.createCell(1).setCellValue(entry.getDescription());
-                row.createCell(2).setCellValue(entry.getDuration());
+                createDurationCell(sheet, row, 2, entry.getDuration());
             }
-            autoSizeColumns(sheet, 3);
+            createPieChart(sheet, numberOfRows, numberOfColumns, "Time spent " + project, 1, 2);
+            autoSizeColumns(sheet, numberOfColumns);
         }
     }
 
-    private static Sheet getSheet(Workbook workbook, String project) {
+    private static XSSFSheet getSheet(XSSFWorkbook workbook, String project) {
         String sheetName = "Total Time " +
                 ("without project".equalsIgnoreCase(project)
                         ? project
                         : "Project " + project);
 
         return workbook.createSheet(sheetName);
-    }
-
-    private static void createHeaderRows(Row headerRow) {
-        headerRow.createCell(0).setCellValue("Project");
-        headerRow.createCell(1).setCellValue("Description");
-        headerRow.createCell(2).setCellValue("Duration");
     }
 }
